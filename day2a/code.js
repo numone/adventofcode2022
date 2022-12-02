dd1c20f5cd6da61aab35cdf02c3ea16b656333a8
Chris Hansen <christinjhansen+github@gmail.com>
2022-12-02 08:53:30 -0500

    day 2

---
+++ day2a/code.js
@@ -0,0 +1,50 @@
+import constants from './input.js'
+
+
+function parseInput(input) {
+    const parsedValues = []
+    
+    const lines = input.split('\n')
+    lines.forEach(line => {
+        parsedValues.push(line.split(' '))
+    });
+
+    return parsedValues
+}
+
+function doGameRounds(rounds) {
+    const roundScores = []
+    rounds.forEach(round => {
+        if(round[0] === constants.OPPONENT.ROCK) {
+            if(round[1] === constants.ME.ROCK) {
+                roundScores.push(constants.GAME_RESULT_SCORE.DRAW + constants.SHAPE_BONUS.ROCK)
+            } else if(round[1] === constants.ME.PAPER) {
+                roundScores.push(constants.GAME_RESULT_SCORE.WIN + constants.SHAPE_BONUS.PAPER)
+            } else if(round[1] === constants.ME.SCISSORS) {
+                roundScores.push(constants.GAME_RESULT_SCORE.LOSE + constants.SHAPE_BONUS.SCISSORS)
+            }
+        } else if(round[0] === constants.OPPONENT.PAPER) {
+            if(round[1] === constants.ME.ROCK) {
+                roundScores.push(constants.GAME_RESULT_SCORE.LOSE + constants.SHAPE_BONUS.ROCK)
+            } else if(round[1] === constants.ME.PAPER) {
+                roundScores.push(constants.GAME_RESULT_SCORE.DRAW + constants.SHAPE_BONUS.PAPER)
+            } else if(round[1] === constants.ME.SCISSORS) {
+                roundScores.push(constants.GAME_RESULT_SCORE.WIN + constants.SHAPE_BONUS.SCISSORS)
+            }
+        } else if(round[0] === constants.OPPONENT.SCISSORS) {
+            if(round[1] === constants.ME.ROCK) {
+                roundScores.push(constants.GAME_RESULT_SCORE.WIN + constants.SHAPE_BONUS.ROCK)
+            } else if(round[1] === constants.ME.PAPER) {
+                roundScores.push(constants.GAME_RESULT_SCORE.LOSE + constants.SHAPE_BONUS.PAPER)
+            } else if(round[1] === constants.ME.SCISSORS) {
+                roundScores.push(constants.GAME_RESULT_SCORE.DRAW + constants.SHAPE_BONUS.SCISSORS)
+            }
+        }
+    })
+
+    return roundScores
+}
+
+const roundValues = parseInput(constants.MY_INPUT)
+const roundResults = doGameRounds(roundValues)
+console.log(roundResults.reduce((a, b) => a + b, 0))
---
+++ day2b/code.js
@@ -0,0 +1,54 @@
+import constants from './input.js'
+
+
+function parseInput(input) {
+    const parsedValues = []
+    
+    const lines = input.split('\n')
+    lines.forEach(line => {
+        parsedValues.push(line.split(' '))
+    });
+
+    return parsedValues
+}
+
+function doGameRounds(rounds) {
+    const roundScores = []
+    // X = lose
+    // Y = draw
+    // Z = win
+
+    rounds.forEach(round => {
+        if(round[0] === constants.OPPONENT.ROCK) {
+            if(round[1] === constants.ME.LOSE) {
+                roundScores.push(constants.GAME_RESULT_SCORE.LOSE + constants.SHAPE_BONUS.SCISSORS)
+            } else if(round[1] === constants.ME.DRAW) {
+                roundScores.push(constants.GAME_RESULT_SCORE.DRAW + constants.SHAPE_BONUS.ROCK)
+            } else if(round[1] === constants.ME.WIN) {
+                roundScores.push(constants.GAME_RESULT_SCORE.WIN + constants.SHAPE_BONUS.PAPER)
+            }
+        } else if(round[0] === constants.OPPONENT.PAPER) {
+            if(round[1] === constants.ME.LOSE) {
+                roundScores.push(constants.GAME_RESULT_SCORE.LOSE + constants.SHAPE_BONUS.ROCK)
+            } else if(round[1] === constants.ME.DRAW) {
+                roundScores.push(constants.GAME_RESULT_SCORE.DRAW + constants.SHAPE_BONUS.PAPER)
+            } else if(round[1] === constants.ME.WIN) {
+                roundScores.push(constants.GAME_RESULT_SCORE.WIN + constants.SHAPE_BONUS.SCISSORS)
+            }
+        } else if(round[0] === constants.OPPONENT.SCISSORS) {
+            if(round[1] === constants.ME.LOSE) {
+                roundScores.push(constants.GAME_RESULT_SCORE.LOSE + constants.SHAPE_BONUS.PAPER)
+            } else if(round[1] === constants.ME.DRAW) {
+                roundScores.push(constants.GAME_RESULT_SCORE.DRAW + constants.SHAPE_BONUS.SCISSORS)
+            } else if(round[1] === constants.ME.WIN) {
+                roundScores.push(constants.GAME_RESULT_SCORE.WIN + constants.SHAPE_BONUS.ROCK)
+            }
+        }
+    })
+
+    return roundScores
+}
+
+const roundValues = parseInput(constants.MY_INPUT)
+const roundResults = doGameRounds(roundValues)
+console.log(roundResults.reduce((a, b) => a + b, 0))
